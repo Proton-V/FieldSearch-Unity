4bfc451dab225ca86dba2b68ad47d76f09e517d6
Proton-V <65833201+Proton-V@users.noreply.github.com>
2022-08-31 05:06:04 +0400

    Add demo scene && scripts

---
+++ Assets/FieldSearch/Demo/Scripts/SampleSearchableMonoBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace FieldSearch.Samples
+{
+    public class SampleSearchableMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Transform Receiver;
+        [SerializeField] private Transform Sender;
+    }
+}
---
+++ Assets/FieldSearch/Demo/Scripts/SampleSearchableMonoBehaviourEditor.cs
@@ -0,0 +1,17 @@
+﻿using FieldSearch.Editor;
+using UnityEditor;
+
+namespace FieldSearch.Samples.Editor
+{
+	[CustomEditor(typeof(SampleSearchableMonoBehaviour))]
+	public class SampleSearchableMonoBehaviourEditor : SearchableEditor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			// Your code
+			// ...
+		}
+	}
+}
