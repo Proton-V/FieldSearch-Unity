e42399d2b18f58315f70c847fc208c869d0a6caf
Proton-V <65833201+Proton-V@users.noreply.github.com>
2022-09-29 22:57:43 +0400

    Add base class for SampleSearchableMonoBehaviour

--- FieldSearch/Assets/FieldSearchDemo/Scripts/SampleSearchableMonoBehaviour.cs
+++ FieldSearch/Assets/FieldSearchDemo/Scripts/SampleSearchableMonoBehaviour.cs
@@ -5,7 +5,7 @@ namespace FieldSearch.Samples
     /// <summary>
     /// Sample monobehaviour with specific inspector
     /// </summary>
-    public class SampleSearchableMonoBehaviour : MonoBehaviour
+    public class SampleSearchableMonoBehaviour : BaseSampleSearchableMonoBehaviour
     {
         [SerializeField] private Transform Receiver;
         [SerializeField] private Transform Sender;
